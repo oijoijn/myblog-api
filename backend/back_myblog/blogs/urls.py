e28e5206017d17a0d3f6a9ca2e2c00ce9eb9fad0
soto <syuweb522@gmail.com>
2025-01-27 19:37:44 +0900

    PostgreSQLにデータベースを変更

--- backend/back_myblog/blogs/urls.py
+++ backend/back_myblog/blogs/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'blogs' 
 
 urlpatterns = [
-    path('', views.BlogListAPI.as_view(), name='blog-list'),
-    path('<int:id>/', views.BlogDetailAPI.as_view(), name='blog-detail'),
-    path('<int:id>/comments/', views.CommentCreateAPI.as_view(), name='comment-create'),
-    path('comments/<int:pk>/', views.CommentEditAPI.as_view(), name='comment-edit'),
-    path('comments/', views.CommentListAPI.as_view(), name='comment-list'),
+    path('list/', views.BlogListAPI.as_view(), name='blog-list'),
+    path('<int:id>/detail/', views.BlogDetailAPI.as_view(), name='blog-detail'),
+    path('<int:id>/comments/create/', views.CommentCreateAPI.as_view(), name='comment-create'),
+    path('<int:pk>/comments/edit/', views.CommentEditAPI.as_view(), name='comment-edit'),
+    path('user/comments/list/', views.CommentListAPI.as_view(), name='comment-list'),
     path('docs/', views.DocsView.as_view(), name='docs'),
 ]
