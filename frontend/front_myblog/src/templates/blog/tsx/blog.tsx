d23121b03d6ec74ff5361920867d75e76ec27cae
soto <syuweb522@gmail.com>
2025-03-08 22:02:44 +0900

    タブ名とblogタイトルの2重表示を訂正

--- frontend/front_myblog/src/templates/blog/tsx/blog.tsx
+++ frontend/front_myblog/src/templates/blog/tsx/blog.tsx
@@ -5,7 +5,6 @@ export const blog: React.FC = () => {
     return (
         <>
             <Box style={containerStyle}>
-                <Typography variant="h3" align="center">サイトについて</Typography>
                 <Typography component="p">
                     このサイトでは、筆者が興味深いと感じた技術や実際に触れたことのある技術についてご紹介させていただいております。可能な限り正確な情報をお届けできるよう努めておりますが、万一誤りがございましたら、下記のコメント欄にてご指摘いただけますと幸いです。
                 </Typography>
