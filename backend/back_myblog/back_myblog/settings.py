a11e7a66be858fc019587904643a7f8aaac0a04f
soto <syuweb522@gmail.com>
2025-03-08 15:47:49 +0900

    nginxのconfとsettings.pyの微調整

--- backend/back_myblog/back_myblog/settings.py
+++ backend/back_myblog/back_myblog/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # プロジェクトルートの static ディレクトリ
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
